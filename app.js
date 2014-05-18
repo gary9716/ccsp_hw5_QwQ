4e7a095071a146a7e3d784725de47911c8fb6d61
tsuKiNo <gary9716@gmail.com>
2014-05-19 06:28:53 +0800

    remaining part:Heroku

--- app.js
+++ app.js
@@ -10,11 +10,14 @@ var flash = require('connect-flash');
 var mongoose = require('mongoose');
 var passport = require('passport');
 
-// require('./config/db'); // TODO [DB] : Connect to database
-// require('./config/passport'); // TODO [FB] : Passport configuration
+var mongoConfig = require('./config/db'); // TODO [DB] : Connect to database
+var passportConfig = require('./config/passport'); // TODO [FB] : Passport configuration
 
 var app = express();
-// var Vote = mongoose.model('Vote'); // TODO [DB] : Get Vote model
+
+var Vote = mongoose.model('Vote'); // TODO [DB] : Get Vote model
+
+var numOfLevels = 7;
 
 // all environments
 app.set('port', process.env.PORT || 3000);
@@ -26,6 +29,7 @@ app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
 app.use(express.cookieParser(process.env.COOKIE_SECRET));
+//app.use(express.cookieParser("ajkldsasdfjaksfjhakwejwaegfkjfaaskllzkjx"));
 app.use(express.session());
 
 // https://github.com/jaredhanson/passport#middleware
@@ -52,28 +56,33 @@ app.post('/vote', function(req, res, next){
   // Stores the voted option (conveted to number) into session
   req.session.vote = +req.body.vote;
 
-  res.redirect('/result');
+  //test code
+  //res.redirect('/result');
 
   /* TODO [FB] : Redirect to passport auth url! */
   // Directly invoke the passport authenticate middleware.
   // Ref: http://passportjs.org/guide/authenticate/
   //
-  // passport.authenticate('facebook')(req, res, next);
+
+  passport.authenticate('facebook')(req, res, next); //authenticate in facebook.com
 });
 
 // TODO [FB]: Facebook callback handler
 // Ref: https://github.com/jaredhanson/passport-facebook/blob/master/examples/login/app.js#L100
 //
-// app.get('/fbcb', passport.authenticate('facebook', {
-//   successRedirect:'/result',
-//   failureRedirect: '/'
-// }));
+app.get('/fbcb', passport.authenticate('facebook', {
+   successRedirect:'/result', //successfully get access token
+   failureRedirect: '/' //failed to get access token from user
+}));
+
+var statisticalResult = [];
+var totalVotes = 0;
 
 app.get('/result', function(req, res){
 
   var vote = req.session.vote, // The voted item (0~6)
-      fbid = "" + Math.random();    // Facebook ID. (Fake)
-      // fbid = req.user && req.user.id; // TODO [FB]: Get user from req.user
+      //fbid = "" + Math.random();    // Facebook ID. (Fake)
+      fbid = req.user && req.user.id; // TODO [FB]: Get user from req.user
 
   // Delete the stored session.
   //
@@ -94,23 +103,70 @@ app.get('/result', function(req, res){
   */
 
   //
-  // var vote = new Vote({vote: vote, fbid: fbid});
-  // vote.save(function(err, newVote){
-  //   if( err ){
-  //     req.flash('info', "你已經投過票囉！");
-  //     return res.redirect('/');
-  //   }
+  var vote = new Vote({vote: vote, fbid: fbid});
+  vote.save(function(err, newVote){
+      
+      if( err ){ //conflict in fbid
+        req.flash('info', "你已經投過票囉！");
+        return res.redirect('/');
+      }
+      
   //
-  //   ... ...
+  //   Get all votes and do the statistics
   //
-       res.render('result', {
-         votes: [18.1, 12.5, 42.44445, 21.3, 1.3, 2.5, 1.85555] // Percentages
-       });
+
+      if(totalVotes == 0) { //initialize
+        for(var i = 0;i < numOfLevels;i++) {
+          statisticalResult.push(0);
+        }
+
+        //async
+        Vote.find(function(err, allVotes) {
+          //console.log(allVotes);
+          if(!err) { 
+            var count = allVotes.length;
+
+            //console.log(allVotes);
+
+            for(var i = 0;i < count;i++) {
+              statisticalResult[allVotes[i].vote]++;
+            }
+
+            doRendering(statisticalResult,count);
+          }
+          else {
+            doRendering([0,0,0,0,0,0,0],1);
+          }
+        });
+
+      }
+      else {
+        totalVotes++;
+        statisticalResult[newVote.vote]++;
+        doRendering(statisticalResult,totalVotes);
+      }
+      
+      function doRendering(statisticalResult,totalVotes) {
+        var renderedResult = [];
+        for(var i=0;i<numOfLevels;i++) {
+          renderedResult.push((statisticalResult[i]/totalVotes)*100);
+        }
+
+        res.render('result', {
+          votes: renderedResult
+        });
+      }
+
+      // test code
+      // res.render('result', {
+      //    votes: [18.1, 12.5, 42.44445, 21.3, 1.3, 2.5, 1.85555] // Percentages
+      // });
   //
-  // });
+  });
 
 });
 
+//start server
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
--- config/passport.js
+++ config/passport.js
@@ -1,7 +1,10 @@
 var passport = require('passport');
 var FacebookStrategy = require('passport-facebook').Strategy;
 
+//let facebook know where it need to redirect to
 passport.use(new FacebookStrategy({
+  //clientID: "295942617240131",
+  //clientSecret: "a6299e6f60d4af0f996b9e942c0aa15b",
   clientID: process.env.FB_APP_ID,
   clientSecret: process.env.FB_APP_SECRET,
   callbackURL: "/fbcb"
@@ -10,7 +13,8 @@ passport.use(new FacebookStrategy({
   // http://passportjs.org/guide/profile/
   // Once the profile is stored in session, it will be available in req.user.
   //
-  done(null, profile);
+
+  done(null, profile); //redirect to success route specified in json's property "successRedirect"
 }));
 
 
